031419c8c29a5bf7733342299b5a6c659a52b68a
Tom <thomas.price15@imperial.ac.uk>
2016-05-23 21:48:13 +0100

    changed getoperand2 to take an argument for immediate/register addressing

--- src/getOperand2.c
+++ src/getOperand2.c
@@ -2,10 +2,23 @@
 // Created by drspaceship on 23/05/16.
 //
 #include "getOperand2.h"
+#include "util.h"
 
-// PRE: instr is a dataProcess instruction
+// PRE: instr is a dataProcess or dataTransfer instruction
 // POST: returns a 32 bit int for operand2 as described in spec
 
-uint32_t getOperand2(uint32_t instr) {
+uint32_t getOperand2(uint32_t instr, bool immediate) {
 
+    uint32_t operand2;
+
+    if (immediate) {
+        uint32_t rotAmount = extractBits(instr, 11, 8) * 2;
+        uint32_t imVal = extractBits(instr, 7, 0);
+        operand2 = binaryShift(imVal, ROR, rotAmount).result;
+    }
+    else {
+        
+    }
+
+    return operand2;
 }
--- src/getOperand2.h
+++ src/getOperand2.h
@@ -2,10 +2,12 @@
 // Created by drspaceship on 23/05/16.
 //
 #include <stdint.h>
+#include <stdbool.h>
+#include "util.h"
 
 #ifndef ARM11_39_GETOPERAND2_H
 #define ARM11_39_GETOPERAND2_H
 
 #endif //ARM11_39_GETOPERAND2_H
 
-uint32_t getOperand2(uint32_t instr);
+uint32_t getOperand2(uint32_t instr, bool immediate);
