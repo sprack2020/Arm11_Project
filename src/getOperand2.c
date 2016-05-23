997b78c655ceacbd98a846c1ef093c21eb1b2154
Tom <thomas.price15@imperial.ac.uk>
2016-05-23 14:52:12 +0100

    data process yo

---
+++ src/getOperand2.c
@@ -0,0 +1,11 @@
+//
+// Created by drspaceship on 23/05/16.
+//
+#include "getOperand2.h"
+
+// PRE: instr is a dataProcess instruction
+// POST: returns a 32 bit int for operand2 as described in spec
+
+uint32_t getOperand2(uint32_t instr) {
+
+}
---
+++ src/getOperand2.h
@@ -0,0 +1,11 @@
+//
+// Created by drspaceship on 23/05/16.
+//
+#include <stdint.h>
+
+#ifndef ARM11_39_GETOPERAND2_H
+#define ARM11_39_GETOPERAND2_H
+
+#endif //ARM11_39_GETOPERAND2_H
+
+uint32_t getOperand2(uint32_t instr);
