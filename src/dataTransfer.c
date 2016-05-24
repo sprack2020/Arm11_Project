ea52e2b8b63bb9f90474abc5a0171e128958301c
Shiraz Butt <sb4515@ic.ac.uk>
2016-05-24 02:01:06 +0100

    ammened calculate offset to take the whole instruction rather than first extracting the offset bits

--- src/dataTransfer.c
+++ src/dataTransfer.c
@@ -12,14 +12,14 @@ void store(uint32_t Rn, uint32_t Rd);
 // PRE: instr is a data transfer instruction
 // behavior: Loads/stores access of memory as described in spec
 void dataTransfer(uint32_t instr) {
-    // get I, l, u, [Rn], Rd, P, offset
+    // get I, l, u, Rn, Rd, P, offset
     uint32_t isImmediateOffset = extractBit(instr, IMM_BIT);
     uint32_t isPreIndex = extractBit(instr, PREPOST_BIT);
     uint32_t isUp = extractBit(instr, UP_BIT);
     uint32_t isLoad = extractBit(instr, LOAD_BIT);
     uint32_t Rn = extractBits(instr, Rn_UPPER, Rn_LOWER);
     uint32_t Rd = extractBits(instr, Rd_UPPER, Rd_LOWER);
-    uint32_t offsetBits = extractBits(instr, OFFSET_UPPER, OFFSET_LOWER);
+    uint32_t offset = calculateOffset(instr);
 
     // perform data transfer
     if (isLoad) {
@@ -30,10 +30,6 @@ void dataTransfer(uint32_t instr) {
 
     // If instruction is post index, add / subtract the offset to base register
     if (!isPreIndex) {
-        // calculate offset
-        uint32_t offset = calculateOffset(offsetBits);
-
-        // add / subtract offset
         if (isUp) {
             Rn += offset;
         } else {
@@ -45,8 +41,8 @@ void dataTransfer(uint32_t instr) {
 }
 
 // calculates and returns the offset to add to the base register
-uint32_t calculateOffset(uint32_t offsetBits) {
+uint32_t calculateOffset(uint32_t instr) {
     // handling offset bits is equivalent to getting second operand in
     // data process, but with the immediate offset flag negated
-    return getOperand2(offsetBits, ! (bool) isImmediateOffset);
+    return getOperand2(instr, ! (bool) isImmediateOffset);
 }
