ca2195f2e17f91161d20907d97dbaae26d6a24e3
Sarthak Kumar <sarthak.kumar15@imperial.ac.uk>
2016-05-24 06:38:08 +0100

    Update iMultiply.c

--- src/iMultiply.c
+++ src/iMultiply.c
@@ -12,14 +12,14 @@ void iMultiply(uint32_t instr) {
         rn = extractBits(instr, 23, 20)
         rs = extractBits(instr, 19, 16)
         rm = extractBits(instr, 27, 24)
-        if (extractBits(instr, 13, 13) != 0) {
+        if (extractBit(instr, 13) != 0) {
             //assuming extractBits returns a uint32_t
             //rd = rm*rs + rn
         }
         else {
             //rd = rm*rs
         }
-        if (extractBits(instr, 12, 12) != 0) {
+        if (extractBit(instr, 12) != 0) {
             //r16[31] = rd[31]
             //if (rd == 0) then r16[30] = 1 else r16[30] = 0;
         }
