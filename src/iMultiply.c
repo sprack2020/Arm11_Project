3e271ba7f7834a52d155dc117648a3a2615a1b90
Sarthak Kumar <sarthak.kumar15@imperial.ac.uk>
2016-05-24 12:57:17 +0100

    Update iMultiply.c

--- src/iMultiply.c
+++ src/iMultiply.c
@@ -8,16 +8,15 @@
 
 void iMultiply(uint32_t instr) {
     if (checkCond(instr)) {
-        rd = extractBits(instr, 11, 8)
-        rn = extractBits(instr, 23, 20)
-        rs = extractBits(instr, 19, 16)
-        rm = extractBits(instr, 27, 24)
+        uint32_t rd = extractBits(instr, 11, 8); uint32_t rdp = &rd;
+        uint32_t rn = extractBits(instr, 23, 20); uint32_t rnp = &rn;
+        uint32_t rs = extractBits(instr, 19, 16); uint32_t rsp = &rs;
+        uint32_t rm = extractBits(instr, 27, 24); uint32_t rmp = &rm;
         if (extractBit(instr, 13) != 0) {
-            //assuming extractBits returns a uint32_t
-            //rd = rm*rs + rn
+            *rdp = *rmp * *rsp + *rnp;
         }
         else {
-            //rd = rm*rs
+            *rdp = *rmp * *rsp;
         }
         if (extractBit(instr, 12) != 0) {
             //r16[31] = rd[31]
