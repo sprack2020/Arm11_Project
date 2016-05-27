d5ce072e8747c4a9da4b3c8a5e7b81cc3bf936c9
Andy Hume <ah2814@ic.ac.uk>
2016-05-27 19:28:15 +0100

    implemented writeInstrs, swapEndianness

---
+++ src/assemble.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "io.c"
+
+int main(int argc, char **argv) {
+    //check for correct number of arguments
+    if (argc != 3) {
+        fprintf(stderr, "Usage: assemble <source file> <output file>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //open source file in text mode
+    FILE* src = openFile(argv[1], "rt");
+
+    char** lines = readLines(src);
+    fclose(src);
+
+    //TODO: turn lines into instructions, somehow
+
+    FILE* out = openFile(argv[2], "wb");
+    if (writeInstrs(out, NULL, 0)) {
+        fprintf(stderr, "Error writing binary file");
+    }
+    fclose(out);
+
+    free(lines);
+    return EXIT_SUCCESS;
+}
