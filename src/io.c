69c7ef6fb1dad6039e9e08d1a7bebf21392a93a9
Andy Hume <ah2814@ic.ac.uk>
2016-05-27 19:04:17 +0100

    fix bugs, move files around

--- src/io.c
+++ src/io.c
@@ -3,17 +3,19 @@
 #define MAXLINE 512 //maximum line length including newline/EOF
 
 //open the source file in read only text-mode, and report any errors
-FILE* openFile(const char* path, const char* mode) {
-    FILE *filePtr = fopen(path, mode);
+FILE* openFile(char* path, char* mode) {
+    FILE* filePtr = fopen(path, mode);
     if (filePtr == NULL) {
         fprintf(stderr, "Error opening source file: %s\n", path);
         exit(EXIT_FAILURE);
     }
+
+    return filePtr;
 }
 
 //count the number of lines in a file.
 int countLines(FILE* file) {
-    char* lineBuff = *malloc(sizeof(char) * MAXLINE);
+    char* lineBuff = (char*) malloc(sizeof(char) * MAXLINE);
     int numLines = 0;
 
     //while fgets does not return a NULL pointer (indicating EOF)
@@ -26,7 +28,7 @@ int countLines(FILE* file) {
 
 //read all the lines (delimited by \n) in file to an array of strings
 char** readLines(FILE* file) {
-    const int numLines = readLines(file);
+    int numLines = countLines(file);
     char** lines = malloc(sizeof(char*) * numLines);
 
     for (unsigned int i = 0; i < numLines; ++i) {
--- src/io.h
+++ src/io.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-FILE* openFile(const char* path, const char* mode);
+FILE* openFile(char* path, char* mode);
 char** readLines(FILE* file);
 
 #endif //ARM11_IO_H
