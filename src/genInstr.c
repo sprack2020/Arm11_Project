677a94625f2bb15fbc7bb3c1cb1c628ce9a0be0f
Andy Hume <ah2814@ic.ac.uk>
2016-05-29 17:03:57 +0100

    implement data processing instruction generator

--- src/genInstr.c
+++ src/genInstr.c
@@ -25,18 +25,35 @@ typedef enum {
 
 //bit positioning constants
 #define CONDSHIFT 27
+#define ISHIFT 24
+#define OPCODESHIFT 20
 #define ASHIFT 20
 #define SSHIFT 19
 #define RNSHIFT 15
 #define RDSHIFT 11
 #define RSSHIFT 7
 #define RMSHIFT 0
+#define OP2SHIFT 0
 
-//non-input bits mask for a multiply instruction
+//non-input bit masks (bits that are always have the same value)
+//for various instructions
+#define DPMASK AL << CONDSHIFT
 #define MULMASK AL << CONDSHIFT | 0x9 << 4
 
-//generate a multiply instruction with S set to 0 and condition of always
-//assumes register arguments are valid (between 0 and 15 inclusive)
+//generate a data processing instruction with S set to 0 and a condition of
+//always. Assumes all field inputs are of appropriate length
+uint32_t genDP(bool immediate, int opcode, long int rn, long int rd, int operand2) {
+    return DPMASK                   |
+           immediate << ISHIFT      |
+           opcode    << OPCODESHIFT |
+           rn        << RNSHIFT     |
+           rd        << RDSHIFT     |
+           operand2  << OP2SHIFT    ;
+
+}
+
+//generate a multiply instruction with S set to 0 and condition of always.
+//assumes all field inputs are of appropriate length
 uint32_t genMul(bool accumulate, long int rd, long int rn,
                 long int rs, long int rm) {
     return MULMASK                      |
@@ -44,7 +61,7 @@ uint32_t genMul(bool accumulate, long int rd, long int rn,
            rd         << RNSHIFT        | //NOTE: in a multiply instruction,
            rn         << RDSHIFT        | //the rd and rn fields are swapped
            rs         << RSSHIFT        |
-           rm         << RMSHIFT;
+           rm         << RMSHIFT        ;
 
 }
 
