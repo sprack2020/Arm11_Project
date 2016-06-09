94d496e123250d0351467f433548de2b8a1f05a8
Andy Hume <ah2814@ic.ac.uk>
2016-06-09 16:53:13 +0100

    fix bug in threeBitCounter where a wait loop would never terminate

--- programs/threeBitCounter.s
+++ programs/threeBitCounter.s
@@ -1,29 +1,34 @@
 ldr r0, =0x20200000
 ldr r1, =0x00001240
 str r1, [r0]
+ldr r1, =0x00040000
+str r1, [r0, #4]
 
-mov r4, #4
-mov r5, #8
-mov r6, #16
+mov r4, #0x4
+mov r5, #0x8
+mov r6, #0x16
+mov r7, #0
 
-loop:
 str r4, [r0, #28]
 str r5, [r0, #28]
 str r6, [r0, #28]
-str r4, [r0, #40]
-str r5, [r0, #40]
-str r6, [r0, #40]
 
+loop:
+orr r8, r4, r5
+orr r8, r8, r6
+str r8, [r0, #40]
+
+ldr r3, =0x00800000
 wait1:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait1
 
 str r4, [r0, #28]
+str r7, [r0, #40]
 
+ldr r3, =0x00800000
 wait2:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait2
@@ -31,34 +36,36 @@ bne wait2
 str r4, [r0, #40]
 str r5, [r0, #28]
 
+ldr r3, =0x00800000
 wait3:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait3
 
 str r4, [r0, #28]
+str r7, [r0, #40]
 
+ldr r3, =0x00800000
 wait4:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait4
 
 str r6, [r0, #28]
-str r5, [r0, #40]
-str r4, [r0, #40]
+orr r8, r5, r4
+str r8, [r0, #40]
 
+ldr r3, =0x00800000
 wait5:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait5
 
 str r4, [r0, #28]
+str r7, [r0, #40]
 
+ldr r3, =0x00800000
 wait6:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait6
@@ -66,16 +73,17 @@ bne wait6
 str r5, [r0, #28]
 str r4, [r0, #40]
 
+ldr r3, =0x00800000
 wait7:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait7
 
 str r4, [r0, #28]
+str r7, [r0, #40]
 
+ldr r3, =0x00800000
 wait8:
-ldr r3, =0x00300000
 sub r3, r3, #1
 cmp r3, #0
 bne wait8
