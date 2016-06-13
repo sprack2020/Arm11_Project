857442346ae8ee3454050dcbc3709bb222812624
drspaceship <thomas.price15@imperial.co.uk>
2016-06-13 03:30:49 +0100

    started compiler

---
+++ src/compilerMain.c
@@ -0,0 +1,21 @@
+//
+// Created by drspaceship on 13/06/16.
+//
+
+// PRE: argv[1] is the input file to be compiled
+// argv[2] is the output file location
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv) {
+
+  if (argc != 2) {
+    puts("Incorrect number of arguments");
+    exit(EXIT_FAILURE);
+  }
+
+  
+
+  exit(EXIT_SUCCESS);
+}
